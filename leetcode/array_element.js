e44af9368f181ac76e847953bb9af044f725f6a2
Scarlett <hao.sijia04@gmail.com>
2023-12-29 16:03:39 -0500

    try

--- leetcode/array_element.js
+++ leetcode/array_element.js
@@ -1,1 +1,10 @@
-
+// 2635 - Apply transformation over each element in Array
+// Given an integer array arr and a mapping function fn
+// return a new array with a transformation applied to each element.
+var map = function (arr, fn) {
+    let returnedArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        returnedArray[i] = fn(arr[i], i);
+    }
+    return returnedArray;
+};
