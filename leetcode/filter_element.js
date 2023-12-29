b7ddc904197a23a58bb46c2160ea884999c8a942
Scarlett <hao.sijia04@gmail.com>
2023-12-29 16:45:02 -0500

    done

--- leetcode/filter_element.js
+++ leetcode/filter_element.js
@@ -1,2 +1,12 @@
 // 2634. Filter Elements from Array
-
+var filter = function (arr, fn) {
+    let newArr = new Array(arr.length);
+    let size = 0;
+    for (let arrElement of arr) {
+        if (fn(arrElement, arr.indexOf(arrElement))) {
+            newArr[size] = arrElement;
+            size++;
+        }
+    }
+    return newArr.slice(0, size);
+};
