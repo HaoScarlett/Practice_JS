6dd2f47d0fa5db86fdde5baab6c811655c112335
Scarlett <hao.sijia04@gmail.com>
2023-12-29 19:19:31 -0500

    output empty

--- leetcode/rm_duplicates.js
+++ leetcode/rm_duplicates.js
@@ -3,17 +3,17 @@ var removeDuplicates = function (nums) {
   // Iterate every element in the array
   // if i = k, i moves forward; i != k, k write and move forward;
   // return k;
-    if (nums.length < 2){
-        return nums;
+  if (nums.length < 2 ) {
+    return nums;
+  } else {
+    for (let i = 1; i < nums.length; i++) {
+      if (nums[i] === nums[writePointer]) {
+        nums[writePointer] = nums[writePointer];
+      } else if (nums[i] !== nums[writePointer]) {
+        writePointer++;
+        nums[writePointer] = nums[i];
+      }
     }
-    else {
-        for (let i = 1; i < nums.length; i++) {
-          if (nums[i] === nums[writePointer]) {
-            nums[writePointer] = nums[writePointer];
-          } else if (nums[i] !== nums[writePointer]) {
-            nums[writePointer] = nums[i];
-          }
-        }
-        return writePointer;
-    }
+    return writePointer;
+  }
 };
