c6a7832c0f6d8020173114312e238a405011f011
Scarlett <hao.sijia04@gmail.com>
2023-12-29 19:27:06 -0500

    read pointer moving range

--- leetcode/rm_duplicates.js
+++ leetcode/rm_duplicates.js
@@ -5,12 +5,12 @@ var removeDuplicates = function (nums) {
   // return k;
   if (nums.length < 2) {
     return nums;
-  } else if ((nums.length = 2 && nums[0] !== nums[1])) {
+  } else if (nums.length === 2 && nums[0] !== nums[1]) {
     return nums;
-  } else if ((nums.length = 2 && nums[0] === nums[1])) {
+  } else if (nums.length === 2 && nums[0] === nums[1]) {
     return nums[0];
   } else if (nums.length > 2) {
-    for (let i = 1; i < nums.length; i++) {
+    for (let i = 1; i < nums.length + 1; i++) {
       if (nums[i] === nums[writePointer]) {
         nums[writePointer] = nums[writePointer];
       } else if (nums[i] !== nums[writePointer]) {
