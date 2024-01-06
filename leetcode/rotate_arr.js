1173fcbced19ce4653ad864e166e67202734766c
Scarlett <hao.sijia04@gmail.com>
2024-01-05 19:50:19 -0500

    Bug. Logic bugs.

--- leetcode/rotate_arr.js
+++ leetcode/rotate_arr.js
@@ -5,15 +5,16 @@ var rotate = function (nums, k) {
   if (k <= 0) {
     return nums;
   }
-  // Write pointer starts from index k
+  // Write pointer starts from index k%nums.length
   // Read pointer i starts from index 0
+  let writer = k % nums.length;
+  let rotatedArr = [];
   for (let i = 0; i < nums.length; i++) {
-    // When i<=k, nums[k] = nums[i]
-    if (i <= k) {
-      nums[k] = nums[i];
-      k++;
+    if (i <= writer) {
+        rotatedArr[writer] = nums[i];
+      writer++;
     }
-    // When i > k, nums[i-k-1] = nums[i]
-    nums[i - k - 1] = nums[i];
+    rotatedArr[i - writer - 1] = nums[i];
   }
+  nums = rotatedArr;
 };
