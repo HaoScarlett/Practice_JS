19ee49f79940cac490a31d35a0ff313fcd5d8e83
Scarlett <hao.sijia04@gmail.com>
2024-01-08 14:14:43 -0500

    Create a new array,not modify nums in place

--- leetcode/rotate_arr.js
+++ leetcode/rotate_arr.js
@@ -1,10 +1,10 @@
 // 189. Rotate Array
 
 var rotate = function (nums, k) {
- // Store two parts of arr seperately
+  // Store two parts of arr seperately
   const length = nums.length;
-  let backwardArr = [nums[0],...nums[k]];
-  let forwardArr = [nums[length - k],...nums[length - 1]];
- // Switch their positions to rotate the elements
+  let backwardArr = nums.slice(0, k);
+  let forwardArr = nums.slice(k);
+  // Switch their positions to rotate the elements
   nums = [forwardArr, backwardArr];
 };
