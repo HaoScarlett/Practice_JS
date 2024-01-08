e3e29f0751ee5c1ae3a5375ce4d23d38d95f82de
Scarlett <hao.sijia04@gmail.com>
2024-01-08 14:23:16 -0500

    Good.Two parts then combine.

--- leetcode/rotate_arr.js
+++ leetcode/rotate_arr.js
@@ -3,8 +3,9 @@
 var rotate = function (nums, k) {
   // Store two parts of arr seperately
   const length = nums.length;
+  k = k % length; // Ensure k is within the array's bounds
   let backwardArr = nums.slice(0, k);
   let forwardArr = nums.slice(k);
   // Switch their positions to rotate the elements
-  nums = [forwardArr, backwardArr];
+  return nums = forwardArr.concat(backwardArr);
 };
