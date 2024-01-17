3e700eed52699503cdd4b4104af98290ebd6ae50
Scarlett <hao.sijia04@gmail.com>
2024-01-17 17:58:10 -0500

    Submit

--- Web-Dev-Ex/Football-Game-Bet/footballBet.js
+++ Web-Dev-Ex/Football-Game-Bet/footballBet.js
@@ -1,14 +1,16 @@
 var maxProfit = function (prices) {
-    let minPricePointer = 0;
-    let currentDay = 1;
-    let currentProfit = 0;
-    for (let i = currentDay; i < prices.length; i++) {
-      if (prices[minPricePointer] > prices[i]) {
-        minPricePointer = i;
-      } else {
-        currentDay = i;
-        currentProfit = prices[i] - prices[minPricePointer];
+  let buyDay = 0;
+  let currentDay = 1;
+  let maxProfit = 0;
+  for (let i = currentDay; i < prices.length; i++) {
+    if (prices[buyDay] > prices[i]) {
+      buyDay = i;
+    } else {
+      currentDay = i;
+      if (prices[i] - prices[buyDay] > maxProfit) {
+        maxProfit = prices[i] - prices[buyDay];
       }
     }
-    return currentProfit;
-  };
+  }
+  return maxProfit;
+};
