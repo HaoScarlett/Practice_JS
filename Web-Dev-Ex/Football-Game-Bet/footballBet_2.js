11474d6f3fe9e36ea8eae8ee2e8c5f43abba6cd6
Scarlett <hao.sijia04@gmail.com>
2024-01-17 18:38:38 -0500

    Run time 75s down to 55s

--- Web-Dev-Ex/Football-Game-Bet/footballBet_2.js
+++ Web-Dev-Ex/Football-Game-Bet/footballBet_2.js
@@ -1,1 +1,14 @@
 // 121. Best Time to Buy and Sell Stock
+var maxProfit = function (prices) {
+  // Initialize minPrice to a large value
+  let minPrice = Infinity;
+  let maxProfit = 0;
+
+  for (let i = 0; i < prices.length; i++) {
+    // Update the minPrice to the minimum of current price and minPrice
+    minPrice = Math.min(minPrice, prices[i]);
+    // Update the maxProfiet to the maximum of current profit and (current profit - minPrice)
+    maxProfit=Math.max(maxProfit,prices[i]-minPrice);
+  }
+  return maxProfit;
+};
