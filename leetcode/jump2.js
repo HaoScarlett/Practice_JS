0db78cd2bc26cb904434ab1c13de390f50ad7e4d
Scarlett <hao.sijia04@gmail.com>
2024-01-22 22:22:58 -0500

    More notes

--- leetcode/jump2.js
+++ leetcode/jump2.js
@@ -1,18 +1,19 @@
-var canJump = function(nums){
-    let length = nums.length;
-    // Initialize the canReach table with false for all indices
-    let canReach = Array(n).fill(false);
-    
-    // Iterate the nums to check every element's reachability
-    for (let i = 0; i < nums.length; i++ ){
-        if(nums[i]>=1){
-            canReach[i]= true;
-        }else{
-            canReach[i]= false
-        }
-        if(canReach[length-1]=== false){
-            return false
-        }
+var canJump = function (nums) {
+  let length = nums.length;
+  // Initialize the canReach table with false for all indices
+  let canReach = Array(length).fill(false);
+  // The first one can always be reached
+  canReach[0] = true;
+
+  // Iterate the nums to check every element's reachability
+  for (let i = 0; i < nums.length; i++) {
+    // If you can reache the current stone,
+    // update the canReach table for the next one
+    if (canReach[i]) {
+      for (let j = 1; j <= nums[i] && i + j < length; j++) {
+        canReach[i + j] = true;
+      }
     }
-    return true;
+  }
+  return canReach[length - 1];
 };
