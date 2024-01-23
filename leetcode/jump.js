d02ca0d1908e5bf343d6d07e0e852245342c4573
Scarlett <hao.sijia04@gmail.com>
2024-01-22 21:15:56 -0500

    Submit fixed 0 issue

--- leetcode/jump.js
+++ leetcode/jump.js
@@ -1,18 +1,25 @@
 // 55. Jump Game
 var canJump = function (nums) {
+  // Handle the edge case
+  if(nums.length <= 1){
+    return true;
+  }
+  
   // Keep track the farthest position you can jump, start from the first index
   let farthest = 0;
+
   for (let i = 0; i < nums.length; i++) {
     if (i + nums[i] > farthest) {
       // Update the farthest position if the current position plus the length is
       // greater than the current farthest position
-      farthest = nums[i] + farthest;
+      // farthest = nums[i] + farthest; -- not just update the current jump length
+      farthest = Math.max(farthest, i + nums[i]);
     }
     // If farthest position is greater or equal to the last index, return true;
-    if (farthest >= nums.length - 1 && farthest < nums.length) {
+    if (farthest >= nums.length - 1 && farthest <= nums.length) {
       return true;
     }
-    // If the loop ends and farthest position doesn't reach the last index, return false.
   }
+  // If the loop ends and farthest position doesn't reach the last index, return false.
   return false;
 };
