5bedba698af81bcaacc317e5fcc857430cb0a508
Scarlett <hao.sijia04@gmail.com>
2024-01-25 17:08:15 -0500

    Submit.For loop condition.

--- leetcode/jump_medium3.js
+++ leetcode/jump_medium3.js
@@ -1,18 +1,21 @@
 // 45. Jump Game 2 Greedy strategy
 var jump = function (nums) {
+  if (nums.length === 1) {
+    return 0;
+  }
   // Initialize the minJump
   let minJump = 0;
   // Set two pointers to tell us the window
   let left = 0;
-  let right = 1;
+  let right = 0;
 
   while (right < nums.length - 1) {
     let farthest = 0;
-    for (let i in (1, r + 1)) {
+    for (let i = 1; i <= right; i++) {
       farthest = Math.max(farthest, nums[i] + i);
-      left = right + 1;
-      right = farthest;
     }
+    left = right + 1;
+    right = farthest;
     minJump += 1;
   }
   return minJump;
