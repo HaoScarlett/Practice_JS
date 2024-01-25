39a0218fd443fd07102a52a2d832465bee5fc2ba
Scarlett <hao.sijia04@gmail.com>
2024-01-25 16:22:06 -0500

    Attention inner loop condition

--- leetcode/jump_medium2.js
+++ leetcode/jump_medium2.js
@@ -1,14 +1,22 @@
 // 45. Junp Game 2
 var jump = function (nums) {
-  const minJump = Array(nums.length);
+  if (nums.length === 1) {
+    return 0;
+  }
+
+  if (nums.length <= 2) {
+    return 1;
+  }
+  const minJump = Array(nums.length).fill(Infinity);
   minJump[0] = 0;
   for (let i = 0; i < nums.length; i++) {
     const maxJump = nums[i];
-    for (let j = 1; 1 <= j <= i + maxJump; j++) {
-      if (nums[j] > i + maxJump) {
-        minJump[j] += 1;
+    for (let j = 1; 1 <= j && j <= i + maxJump; j++) {
+      //   check if the jump from position j can reach the current position i.
+      if (j + nums[j] > i + maxJump) {
+        minJump[j] = Math.min(minJump[j], minJump[i] + 1);
       }
     }
   }
-  return minJump(minJump.length - 1);
+  return minJump[minJump.length - 1];
 };
