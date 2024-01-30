f66693ced32b0f1af4bbe347d2d80d0dec4bbdc2
Scarlett <hao.sijia04@gmail.com>
2024-01-29 22:18:16 -0500

    Simplify

--- leetcode/274_hIndex.js
+++ leetcode/274_hIndex.js
@@ -4,19 +4,22 @@ var hIndex = function (citations) {
   // citations = papers = h
 
   // Handle the edge case
+  // if (citations.length === 1) {
+  //   if (citations[0] === 0) {
+  //     return 0;
+  //   } else {
+  //     return 1;
+  //   }
+  // }
   if (citations.length === 1) {
-    if (citations[0] === 0) {
-      return 0;
-    } else {
-      return 1;
-    }
+    return citations[0] === 0 ? 0 : 1;
   }
-  
+
   // Sort the array in descending order
   citations.sort((a, b) => b - a);
-  
+
   let hIndex = 0;
-  
+
   for (let i = 0; i < citations.length; i++) {
     // h-index can only be as great as their number of publications
     if (citations[i] > i) {
