d4429ddb23e0f5a34dc3caa6d7b38aae2880c0ae
Scarlett <hao.sijia04@gmail.com>
2024-01-29 21:49:51 -0500

    Bug [100] wrong

--- leetcode/274_hIndex.js
+++ leetcode/274_hIndex.js
@@ -2,8 +2,8 @@
 
 var hIndex = function (citations) {
   // citations = papers = h
-  // Sort the array in descending order
-  // When citation is greater or equal to papers, h increment
+
+  // Handle the edge case
   if (citations.length === 1) {
     if (citations[0] === 0 || citations[0] === "") {
       return 0;
@@ -11,13 +11,15 @@ var hIndex = function (citations) {
       return 1;
     }
   }
+  // Sort the array in descending order
+  // When citation is greater or equal to its position, h increment
 
   citations.sort((a, b) => b - a);
 
   let hIndex = 0;
-  
+
   for (let i = 0; i < citations.length - 1; i++) {
-    if (citations[i] >= citations.length) {
+    if (citations[i] >= i) {
       hIndex++;
     }
   }
