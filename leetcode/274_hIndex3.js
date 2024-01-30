848ea41676756fa175ea8127458e28c80ebc81dd
Scarlett <hao.sijia04@gmail.com>
2024-01-30 18:51:20 -0500

    Debug:'for of' loop to iterated the values, instead of indices

--- leetcode/274_hIndex3.js
+++ leetcode/274_hIndex3.js
@@ -8,7 +8,7 @@ var hIndex = function (citations) {
   // Step 1. Count the frequency of each citation
   // if we have a paper of 5 citations, we check if there're
   // at least 5 papers with 5 citations each.
-  for (let citation in citations) {
+  for (let citation of citations) {
     if (citation > n) {
       // If citation is greater than n, count it in the last index
       count[n]++;
@@ -22,7 +22,7 @@ var hIndex = function (citations) {
   for (let i = n; i > 0; i--) {
     sum += count[i];
     // Step 3. Check if the current index satisfies h-index condition
-    if (sum > i) {
+    if (sum >= i) {
       hIndexValue = i;
       break;
     }
