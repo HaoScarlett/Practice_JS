bdb2de3cc0593d03a642d1e4a25ec637750bff88
Scarlett <hao.sijia04@gmail.com>
2024-01-30 15:52:43 -0500

    Bug.Logic flaw. Can't handle duplicates

--- leetcode/274_hIndex2.js
+++ leetcode/274_hIndex2.js
@@ -7,15 +7,12 @@ var hIndex = function (citations) {
   // count each papers citations with current papers
   // Only increment hIndex when citations is greater than papers
   let hIndex = 0;
-  let numOfPaper = citations.length+1;
-  for (let i = 0; i < numOfPaper; i++) {
-    if (citations[i] === 0) {
-      numOfPaper--;
-    } else {
-      if (citations[i] >= numOfPaper) {
-        hIndex++;
-      }
+  let numOfPaper = citations.length + 1;
+  for (let i = 0; i < citations.length; i++) {
+    if (citations[i] >= numOfPaper) {
+      hIndex++;
     }
+      numOfPaper--;//Decrement the number of remaining papers
   }
   return hIndex;
 };
