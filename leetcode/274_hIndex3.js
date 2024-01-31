d616da47571d13f44a213611847a9634ff10ff0b
Scarlett <hao.sijia04@gmail.com>
2024-01-30 19:06:01 -0500

    Clearer comments

--- leetcode/274_hIndex3.js
+++ leetcode/274_hIndex3.js
@@ -8,6 +8,7 @@ var hIndex = function (citations) {
   // Step 1. Count the frequency of each citation
   // if we have a paper of 5 citations, we check if there're
   // at least 5 papers with 5 citations each.
+  //   🧩 Use for...of
   for (let citation of citations) {
     if (citation > n) {
       // If citation is greater than n, count it in the last index
@@ -16,7 +17,11 @@ var hIndex = function (citations) {
     count[citation]++;
   }
 
-  //   Step 2. Accumulate counts backward to find the h-index
+  // Step 2. Accumulate counts backward to find the h-index
+  // We keep doing this, going down from the highest likes to lower ones,
+  // until we find the biggest number where a researcher has at least that many papers,
+  // and each paper has been liked or mentioned at least that many times.
+
   let hIndexValue = 0;
   let sum = 0;
   for (let i = n; i > 0; i--) {
