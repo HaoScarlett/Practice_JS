bdeb87bd607709d2e1e7e77ec831480bbf38016d
Scarlett <hao.sijia04@gmail.com>
2024-01-31 21:21:46 -0500

    Understand the question

---
+++ leetcode/2623_memoize.js
@@ -0,0 +1,19 @@
+// 2623. Memoize
+// Let's build a helper machine 
+// Memoization only works correctly for pure functions
+// Inputs are limited to 3 fns all with numeric inputs.
+// Mind the memory, you could call the fn with dif inputs indefinitely.
+
+// Check if the answer has already knew before call fns
+// Keep track of how many times the same question has been asked
+
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    
+    return function(...args) {
+        
+    }
+}
