83ed41bf27023f1694cff90c07ce4e1851896719
Scarlett <hao.sijia04@gmail.com>
2024-02-07 21:32:37 -0500

    First trial

--- leetcode/134_gasStation.js
+++ leetcode/134_gasStation.js
@@ -12,19 +12,19 @@ var canCompleteCircuit = function (gas, cost) {
   //   Fill the tank in starting point
   let gasInTank = 0;
   // Iterate the gas array in the clockwise direction
-  for (let i = 1; move < totalGas; i++) {
+  for (let i = 1; move <= totalGas; i++) {
     // if (gasInTank - cost[i] < 0) {}
     if (gas[i] > cost[i]) {
       gasInTank = gasInTank + gas[i];
       totalCost = totalCost + cost[i - 1];
       move++;
-      // Check every gas station while keep tracking total gas and cost for the current subarray.
-      // If at any point, the total gas becomes negative, reset the starting station to the next one.
-      if (gasInTank < totalCost) {
-        // if the array has n elements and you are at index i,
-        // the next index in the clockwise direction would be (i + 1) % n.
-        i = (i + 1) % totalGas;
-      }
+    }
+    // Check every gas station while keep tracking total gas and cost for the current subarray.
+    // If at any point, the total gas becomes negative, reset the starting station to the next one.
+    if (gasInTank < totalCost) {
+      // if the array has n elements and you are at index i,
+      // the next index in the clockwise direction would be (i + 1) % n.
+      i = (i + 1) % totalGas;
     }
   }
 };
