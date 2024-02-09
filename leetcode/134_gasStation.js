fc742804c9e9e233a518f35f07bf1cf0285da174
Scarlett <hao.sijia04@gmail.com>
2024-02-08 20:55:54 -0500

    Bug.Test case 1 output -1

--- leetcode/134_gasStation.js
+++ leetcode/134_gasStation.js
@@ -6,31 +6,26 @@ var canCompleteCircuit = function (gas, cost) {
   // Edge case
   // gas.length === cost.length === 1
 
-  let currentCost = [];
   let totalGas = gas.length; // The total amount of gas stations
-  //  Store the total currentGasInTank for each starting point
-  let currentGasInTank = [];
   let totalGasInTank = 0;
   let totalCost = 0;
+  let currentGasInTank = 0;
+  let startingIndex = 0;
 
   // Iterate the gas array in the clockwise direction
   // Check every gas station while keep tracking total gas and cost for the current subarray.
 
   for (i = 0; i < totalGas; i++) {
-    currentGasInTank[0] = gas[i];
-    currentCost[i] = cost[i];
-    totalGasInTank = currentGasInTank.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-    totalCost = currentCost.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-    totalGasInTank = totalGasInTank - totalCost;
+    currentGasInTank += gas[i] - cost[i];
+    totalGasInTank += gas[i] - cost[i];
+    totalCost += cost[i];
     // Check the starting point
     // If at any point, the total gas becomes negative, reset the starting station to the next one.
-    if (totalGasInTank < 0) {
-      return -1;
+    if (currentGasInTank < 0) {
+      startingIndex = i + 1;
+      currentGasInTank = 0;
     }
   }
-  // if (currentGasInTank < 0) {
-  //   return -1;
-  // } else {
-  return totalGas - 1;
-  // }
+  // Check if it's possible to travel around the circuit
+  return totalGasInTank >= totalCost ? (startingIndex % totalGas) : -1;
 };
