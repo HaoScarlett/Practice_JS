29abefc217d8a311caafcb62cd7ccc8221039166
Scarlett <hao.sijia04@gmail.com>
2024-02-08 20:45:05 -0500

    Debug.First station negative,program stops

--- leetcode/134_gasStation.js
+++ leetcode/134_gasStation.js
@@ -15,29 +15,22 @@ var canCompleteCircuit = function (gas, cost) {
 
   // Iterate the gas array in the clockwise direction
   // Check every gas station while keep tracking total gas and cost for the current subarray.
-  let i;
+
   for (i = 0; i < totalGas; i++) {
     currentGasInTank[0] = gas[i];
-    currentGasInTank[i+1] = currentGasInTank + gas[i+1];
-    currentCost[i+1] = cost[i];
+    currentCost[i] = cost[i];
     totalGasInTank = currentGasInTank.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
     totalCost = currentCost.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
     totalGasInTank = totalGasInTank - totalCost;
     // Check the starting point
     // If at any point, the total gas becomes negative, reset the starting station to the next one.
     if (totalGasInTank < 0) {
-      // if the array has n elements and you are at index i,
-      // the next index in the clockwise direction would be (i + 1) % n.
-      i = (i + 1) % totalGas;
-
-      //  🟡 You should reset currentGasInTank and currentCost as you're starting a new subarray.
-      currentGasInTank = [];
-      currentCost = [];
+      return -1;
     }
   }
   // if (currentGasInTank < 0) {
   //   return -1;
   // } else {
-  return i;
+  return totalGas - 1;
   // }
 };
