8da7c782e4d4fa5c7f1c2ec9922445b88273a6e3
Scarlett <hao.sijia04@gmail.com>
2024-02-15 21:04:11 -0500

    bug.[1,3,4,5,2]

---
+++ leetcode/135_candy_2.js
@@ -0,0 +1,30 @@
+var candy = function (ratings) {
+    let n = ratings.length;
+  // everyone has the same rating
+  const allEqual = ratings.every((val, i, arr) => val === arr[0]);
+  if (allEqual === true) {
+    return n;
+  }
+
+  // Start from left side, move right, when encounter a higher rate, give one more candy
+  let candies = new Array(n).fill(1);
+  for (let i = 0; i < n; i++) {
+    if (ratings[i] < ratings[i + 1] && i != n - 1) {
+      candies[i + 1] = candies[i] + 1;
+    }
+  }
+
+  //   Move backward, move left, when encouter a higher rate, give one more candy
+  for (let j = n; j > 0; j--) {
+    if (j > 0 && ratings[j] < ratings[j - 1]) {
+      candies[j - 1] = candies[j] + 1;
+    }
+  }
+
+  // Calculate the total candies
+  const totalSum = candies.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
+  return totalSum;
+};
