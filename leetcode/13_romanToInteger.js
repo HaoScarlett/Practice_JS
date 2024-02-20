a9b8dcfa52fd80684349d9a9f5f4aa933ed0fbea
Scarlett <hao.sijia04@gmail.com>
2024-02-19 21:53:01 -0500

    Intuition

---
+++ leetcode/13_romanToInteger.js
@@ -0,0 +1,11 @@
+// 13. Roman To Integer
+
+// Hints: back to forward; map(callbackFn)
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+  // Left to right: if s[i]< s[i+1], num[i] = s[i+1] - s[i], i++;
+};
