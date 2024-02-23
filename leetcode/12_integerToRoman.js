ba2ae32c708316e7c0b634d43afdf3f03d7e7046
Scarlett <hao.sijia04@gmail.com>
2024-02-22 22:05:00 -0500

    Intuition

---
+++ leetcode/12_integerToRoman.js
@@ -0,0 +1,23 @@
+// 12. Integer To Roman
+
+ /**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function(num) {
+    const romanToInt_map = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1,
+      };
+};
