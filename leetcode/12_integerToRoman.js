7c4107f464472ed5b7e1c809c688bb2fea00969d
Scarlett <hao.sijia04@gmail.com>
2024-02-26 21:08:09 -0500

    Bug

--- leetcode/12_integerToRoman.js
+++ leetcode/12_integerToRoman.js
@@ -1,23 +1,36 @@
 // 12. Integer To Roman
 
- /**
+/**
  * @param {number} num
  * @return {string}
  */
-var intToRoman = function(num) {
-    const romanToInt_map = {
-        M: 1000,
-        CM: 900,
-        D: 500,
-        CD: 400,
-        C: 100,
-        XC: 90,
-        L: 50,
-        XL: 40,
-        X: 10,
-        IX: 9,
-        V: 5,
-        IV: 4,
-        I: 1,
-      };
+var intToRoman = function (num) {
+  const romanNum = [];
+  const romanToInt_map = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+
+  // Look for the largest symbol that fits into map
+  for (const key in romanToInt_map) {
+    const keyValue = romanToInt_map[key];
+    if (num > keyValue) {
+      romanNum.unshift(key);
+      // substract the largest symbol value, and look for the largest symbole fits into the remainder
+      num = num - keyValue;
+    }
+  }
+  // Concatenate romanNum array elements in a string
+  return romanNum.join('');
 };
