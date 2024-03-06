47f60a7d2b1ca37b076967f88ddfb8d1ee2c2bba
Scarlett <hao.sijia04@gmail.com>
2024-03-05 22:10:15 -0500

    Retrive data. Still bug

--- leetcode/383_Ransom_Note_Hash.js
+++ leetcode/383_Ransom_Note_Hash.js
@@ -18,26 +18,27 @@ var canConstruct = function (ransomNote, magazine) {
   }
 
   // Checking each letter in ransomNote with Map
-  for (let noteChar of ransomNote){
-      // if there's no matching key, return false
-      // 🧐 Bug here
-        if (freqMap.has(noteChar)=== false){
-            return false;
-        }
-        // if there's a key correspond with the letter
-        else {
-            // true: check the value
-                // value != 0, decrement 1
-                // value = 0, return false 
-            const frequencies = freqMap.noteChar;
-            if(frequencies != 0){
-                frequencies--;
-            }
-            else{
-                return false;
-            }
-        }
+  for (let noteChar of ransomNote) {
+    // if there's no matching key, return false
+    // 🧐 Bug here
+    // if (freqMap.has(noteChar)=== false)
+    // if (!freqMap.has(noteChar)|| freqMap.get(noteChar) === 0) {
+    //   return false;
+    // }
+    // if there's a key correspond with the letter
+    if (freqMap.has(noteChar)) {
+      // true: check the value
+      // value != 0, decrement 1
+      // value = 0, return false
+      const frequencies = freqMap.get(noteChar);
+      if (frequencies != 0) {
+        freqMap.set(noteChar, frequencies - 1);
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
   }
   // Final check
-
 };
