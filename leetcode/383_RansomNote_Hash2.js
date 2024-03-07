cb26650c87039f539cea44e45dbe92c6167175eb
Scarlett <hao.sijia04@gmail.com>
2024-03-06 21:48:52 -0500

    Submit

--- leetcode/383_RansomNote_Hash2.js
+++ leetcode/383_RansomNote_Hash2.js
@@ -1,2 +1,40 @@
 // 383_RansomNote_Hash2
-var
+var canConstruct = function (ransomNote, magazine) {
+  if (ransomNote.length > magazine.length) {
+    return false;
+  }
+
+  // Build map for two parameters
+  let noteMap = new Map();
+  let magazineMap = new Map();
+
+  // Set objects properties
+
+  // Count occurance of letters in ransomNote
+  for (let char of ransomNote) {
+    if (noteMap.has(char)) {
+      noteMap.set(char, noteMap.get(char) + 1);
+    } else {
+      noteMap.set(char, 1);
+    }
+  }
+
+  //   Count occurence of letters in magazine
+  for (const char of magazine) {
+    if (magazineMap.has(char)) {
+      magazineMap.set(char, magazineMap.get(char) + 1);
+    } else {
+      magazineMap.set(char, 1);
+    }
+  }
+
+  //   For each unique char in ransomNote
+  for (let [char, count] of noteMap) {
+    // check the count of char in the magazineMap >= the count in noteMap
+    const magazineCount = magazineMap.get(char) || 0;
+    if (magazineCount < count) {
+      return false;
+    }
+  }
+  return true;
+};
