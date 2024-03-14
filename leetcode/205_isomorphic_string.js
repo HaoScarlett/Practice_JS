4f07a5b7d9c91e1d5c8194a21da280355fd06be5
Scarlett <hao.sijia04@gmail.com>
2024-03-13 22:38:28 -0400

    BUG. isomorphic strings return false

--- leetcode/205_isomorphic_string.js
+++ leetcode/205_isomorphic_string.js
@@ -5,6 +5,26 @@
  * @param {string} t
  * @return {boolean}
  */
-var isIsomorphic = function(s, t) {
-    
+var isIsomorphic = function (s, t) {
+  // Set up a corresponding map of s, t
+  // If a specific char doesn't in map, set its pair
+  // if it's in the map, check the value
+
+  let isIsomorphic = true;
+  let charMap = new Map();
+
+  // Iterate through s
+  for (const char of s) {
+    for (const charT of t) {
+      if (!charMap.has(char)) {
+        charMap.set(char, charT);
+      } else {
+        // key existed, check the value
+        if (charMap.get(char) !== charT) {
+          isIsomorphic = false;
+        }
+      }
+    }
+  }
+  return isIsomorphic;
 };
