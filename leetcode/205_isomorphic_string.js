08ef5965aa8981aa19b956da888b14c713b1b4bb
Scarlett <hao.sijia04@gmail.com>
2024-03-14 22:49:49 -0400

    Submit. 115ms. Too Slow. Nested for loop

--- leetcode/205_isomorphic_string.js
+++ leetcode/205_isomorphic_string.js
@@ -6,6 +6,26 @@
  * @return {boolean}
  */
 var isIsomorphic = function (s, t) {
+  // Build up a new map to track the char pairs
+  const charNote = new Map();
+
   // Compare the char in two string simultaneously
-  for(let )
+  // char in s is the key, char in t is the value
+  for (let i = 0; i < s.length; i++) {
+    // if it's a new key
+    if (!charNote.has(s[i])) {
+      charNote.set(s[i], t[i]);
+    } else {
+      // it the key exist, check if value matches
+      if (charNote.get(s[i]) !== t[i]) {
+        return false;
+      }
+    }
+    for (const [key, value] of charNote) {
+      if (value === t[i] && key !== s[i]) {
+        return false; // t[i] is already mapped to a different character in s
+      }
+    }
+  }
+  return true;
 };
