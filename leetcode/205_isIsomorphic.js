b2680babf10fb18630b416b8994712f672b8ebb1
Scarlett <hao.sijia04@gmail.com>
2024-03-18 21:48:20 -0400

    Improved version

--- leetcode/205_isIsomorphic.js
+++ leetcode/205_isIsomorphic.js
@@ -1,34 +1,40 @@
 // 205 isIsomorphic strings
 
 var isIsomorphic = function (s, t) {
-  // Convert s to t using hashmap
-  const sKeyMap = new Map();
+  // Convert two strings using hashmap
+  function isMappingValid(s, t) {
+    const map = new Map();
 
-  for (let i = 0; i < s.length; i++) {
-    if (!sKeyMap.has(s[i])) {
-      sKeyMap.set(s[i], t[i]);
-    } else {
-      if (sKeyMap.get(s[i]) !== t[i]) {
-        return false;
+    for (let i = 0; i < s.length; i++) {
+      if (!map.has(s[i])) {
+        map.set(s[i], t[i]);
+      } else {
+        if (map.get(s[i]) !== t[i]) {
+          return false;
+        }
       }
     }
+    return true;
   }
 
-  //   Convert t to s using hashmap
-  const tKeyMap = new Map();
-  for (let j = 0; j < t.length; j++) {
-    if (!tKeyMap.has(t[j])) {
-      tKeyMap.set(t[j], s[j]);
-    } else {
-      if (tKeyMap.get(t[j]) !== s[j]) {
-        return false;
-      }
-    }
-  }
-  return true;
+  // Check if mapping from s to t and t to s are both valid
+  return isMappingValid(s, t) && isMappingValid(t, s);
 };
+//   Convert t to s using hashmap
+//   const tKeyMap = new Map();
+//   for (let j = 0; j < t.length; j++) {
+//     if (!tKeyMap.has(t[j])) {
+//       tKeyMap.set(t[j], s[j]);
+//     } else {
+//       if (tKeyMap.get(t[j]) !== s[j]) {
+//         return false;
+//       }
+//     }
+//   }
+//   return true;
+// };
 
 // Time: iterate through each char in both strings separately, O(n), n = str.length;
 // Space: two maps to store the mapping, O(n);
-// It should work well for small to moderate-sized inputs. 
+// It should work well for small to moderate-sized inputs.
 // For very large inputs, the linear time complexity might become a bottleneck.
