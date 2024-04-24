6c546a173d4628687017ce174626d720700b5430
Scarlett <hao.sijia04@gmail.com>
2024-04-23 21:48:38 -0400

    Ideas

--- leetcode/1_two_sum.js
+++ leetcode/1_two_sum.js
@@ -1,3 +1,16 @@
 // 1_two_sum.js
 
-// 
+// find complement of each num in nums: complement = target - nums[i]
+
+// Use hash map to store key-value pairs for efficient look-ups
+// Key: nums[i], Value: index(i)
+// Notice: hash map key, collision. -- ex:[3,3]
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    
+};
