ef00f996ec80f9f98a3c979348f69a640c6ee191
Scarlett <hao.sijia04@gmail.com>
2024-04-23 22:31:03 -0400

    Update comments and variable names

--- leetcode/1_two_sum.js
+++ leetcode/1_two_sum.js
@@ -1,36 +1,29 @@
 // 1_two_sum.js
 
+/* Finds two indices such that the values at these indices add up to the target.
 // find complement of each num in nums: complement = target - nums[i]
-
-// Use hash map to store key-value pairs for efficient look-ups
+ * Utilizes a hash map to store the values and their indices for efficient lookup. */
 // Key: nums[i], Value: index(i)
 // Notice: hash map key, collision. -- ex:[3,3]
-
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-  // Create a map to note the key-value pairs for further search
-  const numsMap = new Map();
-  //   for (let i = 0; i < nums.length; i++) {
-  //     if (!numsMap.has(nums[i])) {
-  //       numsMap.set(nums[i], i);
-  //     }
-  //   }
+  // Create a map to note the key-value pairs
+  const indexMap = new Map();
 
   // Iterate through the nums, find each num's complement,
   // Search the complement in the rest num to find if there's a matching num
-  for (let j = 0; j < nums.length; j++) {
-    const complement = target - nums[j];
-    // Search complement in numsMap
-    if (numsMap.has(complement)) {
-      // if complement exists, use the indices from the hash map and the current index
-      return [numsMap.get(complement), j];
-    } else {
-      // if complement dosen't exist, add the current ele and its index to the hash map
-      numsMap.set(nums[j], j);
+  for (let index = 0; index < nums.length; index++) {
+    const complement = target - nums[index];
+    // Check if complement exists in numsMap
+    if (indexMap.has(complement)) {
+      // if found, use the indices from the hash map and the current index
+      return [indexMap.get(complement), index];
     }
+    // if not found, add the current ele and its index to the hash map
+    indexMap.set(nums[index], index);
   }
 };
