85d5bfebbde4f2bdc29bd7194181b6fe1ea61371
Scarlett <hao.sijia04@gmail.com>
2024-04-25 22:27:48 -0400

    First version

--- leetcode/202_happyNumber.js
+++ leetcode/202_happyNumber.js
@@ -4,12 +4,32 @@
  * @param {number} n
  * @return {boolean}
  */
-var isHappy = function(n) {
-    // split the number into its individual digits
-    const digits = n.toString().split('').map(Number);
-    // n = 19 digits = [1,9]
-    // use pointer to keep track every digit, when computation is done, update the pointers
+var isHappy = function (n) {
+  // split the number into its individual digits
+  // const digits = n.toString().split('').map(Number);
 
-    // use hash map to keep track of each sum. If a repeated sum exists, it indicates the 
-    // sequence has entered a cycle, return false. 
+  // use pointer to keep track every digit, when computation is done, update the pointers
+  // use hash map to keep track of each sum. If a repeated sum exists, it indicates the
+  // sequence has entered a cycle, return false.
+  const sumCalculated = new Set();
+
+  while (!n === 1) {
+    // n = 19 digits = [1,9]
+    const digits = n.toString().split("").map(Number);
+    // currentSumArray = [1, 81]
+    const currentSumArray = digits.map((Number) => Number ** 2);
+    // 1 + 81 = 82
+    const currentSum = currentSumArray.reduce(
+      (partialSum, currentNum) => partialSum + currentNum,
+      0
+    );
+    // if currentSum isn't in sumCalculated, add it
+    if (!sumCalculated.has(currentSum)) {
+      sumCalculated.add(currentSum);
+    } else {
+        return false;
+        break;
+    }
+    return true;
+  }
 };
