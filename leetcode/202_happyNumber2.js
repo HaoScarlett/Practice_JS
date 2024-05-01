f18cad9a9b3a5839586d80c4deee62d4cb173c94
Scarlett <hao.sijia04@gmail.com>
2024-04-30 22:10:43 -0400

    fast and slow pointers

--- leetcode/202_happyNumber2.js
+++ leetcode/202_happyNumber2.js
@@ -3,8 +3,14 @@
 // Floyd's Cycle-finding
 
 var isHappy = function (n) {
-  // 49 % 10 --> 9 get the last digit
-  for (let i = n.length; i > 0; i--) {
-    const sum = sum + n.slice(-1);
+  function getNextNum(n) {
+    // 49 % 10 --> 9 get the last digit
+    let sum = 0;
+    while (n > 0) {
+      const digit = n % 10;
+      sum += digit ** 2;
+      n = Math.floor(n / 10); // Math.floor(4.9 --> 4)
+    }
+    return sum;
   }
 };
