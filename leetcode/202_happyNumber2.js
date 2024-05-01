74f9c76feb0d1f7aa9a49a859e92ee106417cb11
Scarlett <hao.sijia04@gmail.com>
2024-04-30 22:21:48 -0400

    slow and fast movement

--- leetcode/202_happyNumber2.js
+++ leetcode/202_happyNumber2.js
@@ -3,14 +3,22 @@
 // Floyd's Cycle-finding
 
 var isHappy = function (n) {
-  function getNextNum(n) {
+  function getNextNum(number) {
     // 49 % 10 --> 9 get the last digit
     let sum = 0;
-    while (n > 0) {
-      const digit = n % 10;
+    while (number > 0) {
+      const digit = number % 10;
       sum += digit ** 2;
-      n = Math.floor(n / 10); // Math.floor(4.9 --> 4)
+      number = Math.floor(number / 10); // Math.floor(4.9 --> 4)
     }
     return sum;
   }
+
+  //   Initialize two pointers
+  let slow = n;
+  let fast = getNextNum(n);
+  while (fast !== slow) {
+    
+  }
+  return true;
 };
