b40d2072e103f91406ad3fc5a1215578242cde8a
Scarlett <hao.sijia04@gmail.com>
2024-04-30 22:01:36 -0400

    getNextNum()

---
+++ leetcode/202_happyNumber2.js
@@ -0,0 +1,10 @@
+// 202_happyNumber2
+
+// Floyd's Cycle-finding
+
+var isHappy = function (n) {
+  // 49 % 10 --> 9 get the last digit
+  for (let i = n.length; i > 0; i--) {
+    const sum = sum + n.slice(-1);
+  }
+};
