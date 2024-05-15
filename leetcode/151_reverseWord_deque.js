964f5d363c29c1faaaae591f08851b26fe3073a9
Scarlett <hao.sijia04@gmail.com>
2024-05-14 22:29:28 -0400

    pointers

--- leetcode/151_reverseWord_deque.js
+++ leetcode/151_reverseWord_deque.js
@@ -3,3 +3,12 @@
  * @param {string} s
  * @return {string}
  */
+var reverseWords = function (s) {
+  let left = 0;
+  let right = s.length - 1;
+
+  let wordsList = [];
+  let word = "";
+};
+// charAt
+// while
