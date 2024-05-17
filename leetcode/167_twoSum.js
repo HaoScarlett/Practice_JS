ec71581f24d262e308c4ca54db58c5e45447bf79
Scarlett <hao.sijia04@gmail.com>
2024-05-16 21:46:53 -0400

    Bug. Time limit exceeded

--- leetcode/167_twoSum.js
+++ leetcode/167_twoSum.js
@@ -7,13 +7,14 @@
 var twoSum = function (numbers, target) {
   let index1 = 0;
   let index2 = 1;
-  let sum = numbers[index1] + numbers[index2];
 
+  let sum = numbers[index1] + numbers[index2];
   while (sum !== target) {
     if (index2 < numbers.length) {
       index2++;
+    } else if (index1 < numbers.length - 1) {
+      index1++;
     }
-    index1++;
   }
   return [index1, index2];
 };
