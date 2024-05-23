aab2f0c05b60a13c4f9f187a45f9dd1eeb0f9380
Scarlett <hao.sijia04@gmail.com>
2024-05-22 22:54:50 -0400

    follow the zigzag direction

--- leetcode/6_zigzagConversion.js
+++ leetcode/6_zigzagConversion.js
@@ -7,13 +7,33 @@
 
 // numRows = 1
 // numRows > s.length
-var convert = function(s, numRows) {
-    // Initalize an array of string to store every row's sub string
-    const subStrings = [];
+var convert = function (s, numRows) {
+  // Initalize an array of string to store every row's sub string
+  const subStrings = [];
 
-    // Iterate through the s, every pattern repeat in every 2*(s.length - 1)
-    // Keep track of the turning point to change the direction. Move the pointer to 
-    // the right row
-    let 
+  // Iterate through the s, every pattern repeat in every 2*(s.length - 1)
+  // Keep track of the turning point to change the direction. Move the pointer to
+  // the right row
+  let i = 0;
+  let directionTrack = 0;
 
+  while (i < s.length) {
+    // in every pattern, turning point is 
+    if (directionTrack = numRows) {
+      subStrings[i] = s[i];
+      i++;
+      directionTrack++;
+    } else if (directionTrack === numRows) {
+        subStrings[numRows] = s[i];
+        i++;
+        directionTrack++;
+    } else if (directionTrack > numRows) {
+        subStrings[i - 1] = s[i];
+        i--;
+        directionTrack++;
+        if(directionTrack  === 2*(s.length - 1)){
+            directionTrack = 0;
+        }
+    }
+  }
 };
