a1768e62458b7cfe1890ebfdb43239c0f9e25186
Scarlett <hao.sijia04@gmail.com>
2024-05-22 22:28:24 -0400

    starting point

---
+++ leetcode/6_zigzagConversion.js
@@ -0,0 +1,21 @@
+// 6_zigzagConversion
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+
+// numRows = 1
+// numRows > s.length
+var convert = function(s, numRows) {
+    // Initalize an array of string to store every row's sub string
+    const subStrings = [];
+
+    // Iterate through the s, every pattern repeat in every 2*(s.length - 1)
+    // Keep track of the turning point to change the direction. Move the pointer to 
+    // the right row
+    while(let i < s.length){
+        
+    }
+
+};
