0cc393cc6d41bafe0adcd3674f84981ed957597f
Scarlett <hao.sijia04@gmail.com>
2024-05-24 14:47:52 -0400

    while loop trial

--- leetcode/6_zigzagConversion.js
+++ leetcode/6_zigzagConversion.js
@@ -21,24 +21,26 @@ var convert = function (s, numRows) {
   let goingDown = true;
 
   // loop through the s and append it to the current row
-  for (let i = 0; i < s.length; i++) {
-    if (goingDown === true) {
-      rows[currentRow] = rows[currentRow].concat(s[i]);
+  let index = 0;
+  while (index < s.length) {
+    // Append the current char to the current row
+      rows[currentRow] = rows[currentRow].concat(s[index]);
+      index++;
       currentRow++;
-      if (currentRow === numRows - 1) {
-        goingDown = false;
-      }
+
+    // detect whether the currentRow reach the buttom
+    if (currentRow === numRows - 1) {
+      goingDown = false;
     }
 
     if (goingDown === false) {
       currentRow = currentRow - 1;
-      rows[currentRow] = rows[currentRow].concat(s[i]);
-      // adjust the two pointers as per the zigzag rules
-      // if currentRow = 0, re-start next zigzag pattern
-      if (currentRow === 0) {
-        // currentRow = 0;
-        goingDown = true;
-      }
+      rows[currentRow] = rows[currentRow].concat(s[index]);
+      index++;
+    }
+
+    if (currentRow === 0) {
+      goingDown = true;
     }
   }
 
