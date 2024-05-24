00d7858df1f72ea36c139897bab73234a78b0b7a
Scarlett <hao.sijia04@gmail.com>
2024-05-23 22:18:57 -0400

    first draft

--- leetcode/6_zigzagConversion.js
+++ leetcode/6_zigzagConversion.js
@@ -8,30 +8,36 @@
 // numRows = 1
 // numRows > s.length
 var convert = function (s, numRows) {
+  // handle edge cases
+  if (s.length === 1 || numRows >= s.length) {
+    return s;
+  }
+
   // Initalize an array of string to store every row's sub string
-  const subStrings = [];
+  const rows = Array(numRows).fill("");
 
-  // Iterate through the s, every pattern repeat in every 2*(s.length - 1)
-  // Keep track of the turning point to change the direction. Move the pointer to
-  // the right row
-  let index = 0;
-  let directionTrack = false;
+  //initialize a pointer to track the current row and a tracker to determine the direction
+  const currentRow = 0;
+  const goingDown = true;
 
-  while (index < s.length) {
-    // in every pattern, turning point is 
-    if (index < numRows) {
-      subStrings[] = s[i];
-      index++;
-    } else if (index === numRows) {
-        subStrings[numRows] = s[i];
-        i++;
-        directionTrack = true;
-    } else if (index > numRows) {
-        subStrings[i - 1] = s[i];
-        i--;
-        if(index  === 2*(s.length - 1)){
-            directionTrack = true;
-        }
+  // loop through the s and append it to the current row
+  for (let i = 0; i < s.length; i++) {
+    rows[i] = rows[i].concat(s[i]);
+    if (currentRow === numRows) {
+      goingDown = false;
+    }
+
+    if (goingDown === false) {
+      rows[i - 2] = rows[i - 2].concat(s[i]);
+      // adjust the two pointers as per the zigzag rules
+      // if currentRow = 0, re-start next zigzag pattern
+      if ((i % 2) * (numRows - 1)) {
+        currentRow = 0;
+        goingDown = true;
+      }
     }
   }
+
+  // join all strings in 'rows'
+  return rows.join("");
 };
