35b6836dd8a4f95e0a2a5c19712223c0d04880d3
Scarlett <hao.sijia04@gmail.com>
2024-05-27 22:42:38 -0400

    first draft

--- leetcode/20_valid_parentheses.js
+++ leetcode/20_valid_parentheses.js
@@ -3,7 +3,22 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    // last in, first out
-    const length
+var isValid = function (s) {
+  // last in, first out
+  const length = s.length;
+  const input = Array(length).fill("");
+
+  let i = 0; // pointer
+  while (i < length) {
+    input[i] = s[i];
+    if (s[i] === ")" && input[i - 1] !== "(") {
+      return false;
+    } else if (s[i] === "]" && input[i - 1] !== "[") {
+      return false;
+    } else if (s[i] === ")" && input[i - 1] !== "(") {
+      return false;
+    }
+    i++;
+  }
+  return true;
 };
