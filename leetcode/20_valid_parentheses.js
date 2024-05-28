1ef8b615809063b9190173bdac510cd88d3d6ae7
Scarlett <hao.sijia04@gmail.com>
2024-05-27 22:45:46 -0400

    BUG. (( true

--- leetcode/20_valid_parentheses.js
+++ leetcode/20_valid_parentheses.js
@@ -6,6 +6,10 @@
 var isValid = function (s) {
   // last in, first out
   const length = s.length;
+  if (length === 1 || length % 2 !== 0) {
+    return false;
+  }
+
   const input = Array(length).fill("");
 
   let i = 0; // pointer
