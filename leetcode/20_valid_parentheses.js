6d4037fa67cd199f0e552512be43468b6c2dc766
Scarlett <hao.sijia04@gmail.com>
2024-05-29 19:28:44 -0400

    BUG. ()

--- leetcode/20_valid_parentheses.js
+++ leetcode/20_valid_parentheses.js
@@ -5,32 +5,35 @@
  */
 var isValid = function (s) {
   // last in, first out
-  const length = s.length;
-  if (length === 1 || length % 2 !== 0) {
+  if (s.length === 1 || s.length % 2 !== 0) {
     return false;
   }
 
   // create brackets pairs
   const bracketsPairs = {
-    "(": ")",
-    "{": "}",
-    "[": "]",
+    ")": "(",
+    "}": "{",
+    "]": "[",
   };
 
-  const input = Array(length).fill("");
-
-  let i = 0; // pointer
+  const input = Array(s.length).fill("");
 
   for (const char of s) {
     if (char === "(" || char === "{" || char === "[") {
       // if it's an opening bracket, push it to the array
       input.push(char);
     }
-    if(char === ")" || char === "}" || char === "]"){
-      // if it's an closing bracket, check if it matches the previous one
-      // match: pop the previous one
+    if (char === ")" || char === "}" || char === "]") {
+      // if it's an closing bracket, check if it matches last value in the stack
       // no match: return false
-      if ()
+      if (input.length === 0 || input.pop() !== bracketsPairs[char]) {
+        return false;
+      }
     }
   }
+  if (input.length !== 0) {
+    return false;
+  }
+  return true;
+  // return input.length === 0;
 };
