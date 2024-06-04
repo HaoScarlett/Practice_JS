a2474b3aa4405f0ffa31c6205c5206ad53ac7c85
Scarlett <hao.sijia04@gmail.com>
2024-06-03 22:36:58 -0400

    BUG

--- leetcode/71_simplify_path.js
+++ leetcode/71_simplify_path.js
@@ -4,8 +4,27 @@
  * @param {string} path
  * @return {string}
  */
-var simplifyPath = function(path) {
-    // spllit the input path using '/' as a delimiter
-    // s = '/a/./b/../../c/' s.split('/') => ["", "a", ".", "b", "..", "..", "c", ""]
-    // 
+var simplifyPath = function (path) {
+  // spllit the input path using '/' as a delimiter
+  // s = '/a/./b/../../c/' s.split('/') => ["", "a", ".", "b", "..", "..", "c", ""]
+  // '.' represent the current path, ignore
+  // '..' if there's a previous segment, remove it. if it's at root, igore it
+  // '...' 'a' are valid directory names
+  const pathSegments = path.split("/");
+  //   console.log(pathSegments)
+
+  // use a stack to store valid segments
+  // push the valid directory names
+  // pop the invalid '..' if the stack is not empty
+  const stack = ["/"];
+  for (const name of pathSegments) {
+    if (name !== "" || name !== "." || name !== "..") {
+      stack.push(name);
+    }
+  }
+
+  // rebuild the path
+  return stack.join("/");
 };
+
+// simplifyPath("/a/./b/../../c/");
