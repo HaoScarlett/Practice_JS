02fbf216015638c11e96f6576f3efd9900bbaf66
Scarlett <hao.sijia04@gmail.com>
2024-06-06 21:07:33 -0400

    for in loop

--- leetcode/71_simplify_path2.js
+++ leetcode/71_simplify_path2.js
@@ -1,7 +1,23 @@
 // 71_simplify_path2
 
 var simplifyPath = function (path) {
-    const pathSegments = path.split("/");
-    let stack = [];
-    
-}
+  const pathSegments = path.split("/");
+  let stack = [];
+
+  for (const dir of pathSegments) {
+    if (dir === "" || dir === ".") {
+      continue;
+    } else if (dir === "..") {
+      if (stack.length !== 0) {
+        stack.pop();
+        continue;
+      } else {
+        continue;
+      }
+    } else {
+      stack.push(dir);
+    }
+  }
+
+  return "/" + stack.join("/");
+};
