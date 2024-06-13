aa3acebd333fbcbeef70bff1c05967f40ac1c67b
Scarlett <hao.sijia04@gmail.com>
2024-06-12 22:39:17 -0400

    promise.all

--- leetcode/2723_add2Promises.js
+++ leetcode/2723_add2Promises.js
@@ -6,22 +6,17 @@
  * @return {Promise}
  */
 var addTwoPromises = async function (promise1, promise2) {
+  //   try {
+  //     return (await promise1) + (await promise2);
+  //   } catch (error) {
+  //     console.log(error);
+  //     throw error;
+  //   }
   try {
-    return (await promise1) + (await promise2);
+    const [res1, res2] = await Promise.all([promise1, promise2]);
+    return res1 + res2;
   } catch (error) {
-    console.log(error);
+    console.error(error);
     throw error;
   }
 };
-
-/**
- * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
- *   .then(console.log); // 4
- */
-
-/* const promise1 = Promise.resolve(123);
-
-promise1.then((value) => {
-  console.log(value);
-  // Expected output: 123
-}); */
