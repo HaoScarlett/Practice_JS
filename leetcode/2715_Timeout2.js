78657263453a2fbdfbb6e074eafef501b82fa069
Scarlett <hao.sijia04@gmail.com>
2024-06-18 20:57:09 -0400

    boolean flag approach

--- leetcode/2715_Timeout2.js
+++ leetcode/2715_Timeout2.js
@@ -1,2 +1,13 @@
 // 2715_Timeout2
 
+// use a boolean flag to decide whether calling fn is allowed or not
+
+var cancellable = function (fn, args, t) {
+  let isCancelled = false;
+  const timerId = setTimeout(function () {
+    if (!isCancelled) fn(...args);
+  }, t);
+  return function () {
+    isCancelled = true;
+  };
+};
