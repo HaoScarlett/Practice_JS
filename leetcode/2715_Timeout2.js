298490764310ae37b3c8825a778c01955187b8e3
Scarlett <hao.sijia04@gmail.com>
2024-06-18 21:49:01 -0400

    examples

--- leetcode/2715_Timeout2.js
+++ leetcode/2715_Timeout2.js
@@ -11,3 +11,22 @@ var cancellable = function (fn, args, t) {
     isCancelled = true;
   };
 };
+
+// Examples
+/* const result = [];
+const fn = (x) => x * 5;
+const args = [2],
+  t = 20,
+  cancelTimeMs = 50;
+const start = performance.now();
+const log = (...argsArr) => {
+  const diff = Math.floor(performance.now() - start);
+  result.push({ time: diff, returned: fn(...argsArr) });
+};
+
+const cancel = cancellable(log, args, t);
+const maxT = Math.max(t, cancelTimeMs);
+setTimeout(cancel, cancelTimeMs);
+setTimeout(() => {
+  console.log(result);
+}, maxT + 15); */
