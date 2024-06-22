24afd91055e441299320eb1a91de257be64a5a02
Scarlett <hao.sijia04@gmail.com>
2024-06-21 22:27:36 -0400

    sketch

--- leetcode/155_minStack.js
+++ leetcode/155_minStack.js
@@ -1,39 +1,56 @@
 // 155_minStack
 
-var MinStack = function() {
-    
-};
-
-/** 
- * @param {number} val
- * @return {void}
- */
-MinStack.prototype.push = function(val) {
-    
-};
+class MinStack {
+  constructor() {
+    constructor();
+    {
+      this.stack = [];
+      this.minStack = [];
+    }
+  }
+  /**
+   * @param {number} val
+   * @return {void}
+   */
+  push(val) {
+    this.stack.push(val);
+    // if the minStack is empty or the pushed value is less or equal to the top
+    // of the minStack, push val onto Minstack as wee
+    if (this.minStack.length === 0 || val <= this.minStack.top()) {
+      this.minStack.push(val);
+    }
+  }
+  /**
+   * @return {void}
+   */
+  pop() {
+    if (this.stack.length === 0) {
+      return;
+    }
+    this.stack.pop();
+    // if the poped value is equal to the top of the minStack, pop from minStack as well
+    if (this.stack.pop() === this.minStack.top()) {
+      this.minStack.pop();
+    }
+  }
+  /**
+   * @return {number}
+   */
+  top() {
+    if (this.stack.length === 0) {
+      return;
+    }
+    return this.stack.pop();
+  }
+  /**
+   * @return {number}
+   */
+  getMin() {
+    return this.minStack.pop();
+  }
+}
 
 /**
- * @return {void}
- */
-MinStack.prototype.pop = function() {
-    
-};
-
-/**
- * @return {number}
- */
-MinStack.prototype.top = function() {
-    
-};
-
-/**
- * @return {number}
- */
-MinStack.prototype.getMin = function() {
-    
-};
-
-/** 
  * Your MinStack object will be instantiated and called as such:
  * var obj = new MinStack()
  * obj.push(val)
