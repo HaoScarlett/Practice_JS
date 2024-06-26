7b467d1c0f7491ddfeb7574dab43826b2566784d
Scarlett <hao.sijia04@gmail.com>
2024-06-25 20:45:24 -0400

    set up

---
+++ leetcode/2725_intervalCancellation.js
@@ -0,0 +1,10 @@
+// 2725_intervalCancellation
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
+    
+};
