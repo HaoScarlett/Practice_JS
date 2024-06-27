af36d4a096f8fd095c68aceddb207fdce7d1f8eb
Scarlett <hao.sijia04@gmail.com>
2024-06-26 21:58:54 -0400

    callback to be excuted after the current stack of code has excuted

--- leetcode/2725_intervalCancellation.js
+++ leetcode/2725_intervalCancellation.js
@@ -10,7 +10,7 @@
 var cancellable = function (fn, args, t) {
   let firstTimerId = setTimeout(() => {
     fn(...args);
-  });
+  }, 0);
   let timerId = setInterval(() => {
     fn(...args);
   }, t);
@@ -29,3 +29,32 @@ args is a valid JSON array
 1 <= args.length <= 10
 30 <= t <= 100
 10 <= cancelTimeMs <= 500 */
+
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 2;
+ *  const args = [4], t = 35, cancelTimeMs = 190;
+ *
+ *  const start = performance.now();
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)});
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  setTimeout(cancel, cancelTimeMs);
+ *   
+ *  setTimeout(() => {
+ *      console.log(result); // [
+ *                           //     {"time":0,"returned":8},
+ *                           //     {"time":35,"returned":8},
+ *                           //     {"time":70,"returned":8},
+ *                           //     {"time":105,"returned":8},
+ *                           //     {"time":140,"returned":8},
+ *                           //     {"time":175,"returned":8}
+ *                           // ]
+ *  }, cancelTimeMs + t + 15)    
+ */
