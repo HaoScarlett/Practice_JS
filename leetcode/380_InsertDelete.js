f3784dfeb9c085ca6741459f6ae096ba0e355f7d
Scarlett <hao.sijia04@gmail.com>
2024-06-27 22:06:15 -0400

    confused. O(1) time

--- leetcode/380_InsertDelete.js
+++ leetcode/380_InsertDelete.js
@@ -1,34 +1,48 @@
 // 380_InsertDelete GetRandom O(1)
 
+/* Set: object that lets you store unique values
 
-var RandomizedSet = function() {
-    
+
+
+*/
+var RandomizedSet = function () {
+  const _set = [];
 };
 
-/** 
+/**
  * @param {number} val
  * @return {boolean}
  */
-RandomizedSet.prototype.insert = function(val) {
-    
+RandomizedSet.prototype.insert = function (val) {
+  if (!_set.includes(val)) {
+    return false;
+  } else {
+    _set.push(val);
+    return true;
+  }
 };
 
-/** 
+/**
  * @param {number} val
  * @return {boolean}
  */
-RandomizedSet.prototype.remove = function(val) {
+RandomizedSet.prototype.remove = function (val) {
+  if(!_set.includes(val)){
+    return false
+  } else {
     
+  }
 };
 
 /**
  * @return {number}
  */
-RandomizedSet.prototype.getRandom = function() {
-    
+RandomizedSet.prototype.getRandom = function () {
+  const randomIndex = Math.floor(Math.random() * this.length);
+  return;
 };
 
-/** 
+/**
  * Your RandomizedSet object will be instantiated and called as such:
  * var obj = new RandomizedSet()
  * var param_1 = obj.insert(val)
