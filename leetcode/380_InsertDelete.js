044fc7f78b80c17ed279d5b87f3478573b2978a3
Scarlett <hao.sijia04@gmail.com>
2024-07-02 22:02:16 -0400

    use of two data structure

--- leetcode/380_InsertDelete.js
+++ leetcode/380_InsertDelete.js
@@ -1,46 +1,56 @@
 // 380_InsertDelete GetRandom O(1)
 
 /* Set: object that lets you store unique values
-
+Insertion removal random all O(1)
 
 
 */
-var RandomizedSet = function () {
-  this._set = []; // define _sett as a property of the obj
-};
-
-/**
- * @param {number} val
- * @return {boolean}
- */
-RandomizedSet.prototype.insert = function (val) {
-  if (!this._set.includes(val)) {
-    return false;
-  } else {
-    this._set.push(val);
+class RandomizedSet {
+  constructor() {
+    this.list = []; // define _sett as a property of the obj
+    this.dict = {};
+  }
+  /**
+   * @param {number} val
+   * @return {boolean}
+   */
+  insert(val) {
+    if (!this.dict.hasOwnProperty(val)) {
+      return false;
+    }
+    this.dict[val] = this.list.length;
+    this.list.push(val);
     return true;
   }
-};
-
-/**
- * @param {number} val
- * @return {boolean}
- */
-RandomizedSet.prototype.remove = function (val) {
-  // swap and pop
-  // Delete a value in an arr in O(1)
-  const index = this._set.indexOf(val);
-  if (index !== -1) {
-    this._set[index] = this._set[this._set.length - 1];
-    this._set.pop();
+  /**
+   * @param {number} val
+   * @return {boolean}
+   */
+  remove(val) {
+    // swap and pop
+    // Delete a value in an arr in O(1)
+    // const index = this._list.indexOf(val);
+    // if (index !== -1) {
+    //   this._list[index] = this._list[this._list.length - 1];
+    //   this._list.pop();
+    // }
+    if (this.dict.hasOwnProperty(val)) {
+      const lastElement = this.list[this.list.length - 1];
+      const idx = this.dict[val];
+      this.list[idx] = lastElement;
+      this.dict[lastElement] = idx;
+      // delete the last ele
+      this.list.pop();
+      delete this[val];
+      return true;
+    }
+    return false;
   }
-};
-
-/**
- * @return {number}
- */
-RandomizedSet.prototype.getRandom = function () {
-  const randomIndex = Math.floor(Math.random() * this._set.length);
-  return this._set[randomIndex];
-};
-
+  /**
+   * @return {number}
+   */
+  getRandom() {
+    const randomIndex = Math.floor(Math.random() * this.list.length);
+    return this.list[randomIndex];
+  }
+}
