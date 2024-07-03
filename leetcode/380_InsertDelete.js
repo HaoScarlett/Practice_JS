678dab1ee13850763c62b2a1f58270dbb941d1e4
Scarlett <hao.sijia04@gmail.com>
2024-07-02 22:05:48 -0400

    Bug fixed

--- leetcode/380_InsertDelete.js
+++ leetcode/380_InsertDelete.js
@@ -7,7 +7,7 @@ Insertion removal random all O(1)
 */
 class RandomizedSet {
   constructor() {
-    this.list = []; // define _sett as a property of the obj
+    this.list = []; 
     this.dict = {};
   }
   /**
@@ -15,7 +15,7 @@ class RandomizedSet {
    * @return {boolean}
    */
   insert(val) {
-    if (!this.dict.hasOwnProperty(val)) {
+    if (this.dict.hasOwnProperty(val)) {
       return false;
     }
     this.dict[val] = this.list.length;
@@ -28,20 +28,18 @@ class RandomizedSet {
    */
   remove(val) {
     // swap and pop
-    // Delete a value in an arr in O(1)
-    // const index = this._list.indexOf(val);
-    // if (index !== -1) {
-    //   this._list[index] = this._list[this._list.length - 1];
-    //   this._list.pop();
-    // }
+
     if (this.dict.hasOwnProperty(val)) {
       const lastElement = this.list[this.list.length - 1];
       const idx = this.dict[val];
+
+      // swap the ele to be removed with the last ele
       this.list[idx] = lastElement;
       this.dict[lastElement] = idx;
+
       // delete the last ele
       this.list.pop();
-      delete this[val];
+      delete this.dict[val];
       return true;
     }
     return false;
