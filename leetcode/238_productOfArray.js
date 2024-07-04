5d9307f7ee5dea76c75fd7d740b19831dc7eddee
Scarlett <hao.sijia04@gmail.com>
2024-07-03 22:20:18 -0400

    testing

--- leetcode/238_productOfArray.js
+++ leetcode/238_productOfArray.js
@@ -4,6 +4,24 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var productExceptSelf = function(nums) {
-    
-};
+// O(n) time constrain
+// var productExceptSelf = function(nums) {
+
+// };
+
+
+const nums = [1, 2, 3, 4]
+const output = [24, 12, 8, 6]
+const output1 = []
+for (let i = 0; i < 4; i++) {
+    let result = nums[i] * nums[i + 1]
+    // console.log(result)
+    output1.push(result)
+}
+console.log(output1) // [2,6,12,NaN]
+const output2 = []
+for (let j = 4; j > 0; j--) {
+    let num = output1[j] * nums[j-1];
+    output2.push(num)
+}
+console.log(output2) // [ NaN, NaN, 24, 6 ]
