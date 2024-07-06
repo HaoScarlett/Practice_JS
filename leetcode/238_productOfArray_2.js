819db670ecb43827cf6c765a2bcc1538c8bf0692
Scarlett <hao.sijia04@gmail.com>
2024-07-05 22:48:48 -0400

    fix the suffix logic

--- leetcode/238_productOfArray_2.js
+++ leetcode/238_productOfArray_2.js
@@ -2,26 +2,19 @@
 
 var productExceptSelf = function (nums) {
     // Initialize the result array 
-    const result = [];
+    const result = new Array(nums.length).fill(1);
     // Compute the prefix value and store them in result arr
-    let acc = 1;
+    let prefix = 1;
     for (let i = 0; i < nums.length; i++) {
-        acc = acc * nums[i];
-        result[i] = acc;
+        result[i] = prefix;
+        prefix = prefix * nums[i];
     }
 
     // Traverse the nums from right to left to compute the suffix value
-    let currValue = 1;
-    for (let j = nums.length - 1; j > 0; j--) {
-        currValue = currValue * nums[j];
-        // compute the final value
-        if (j + 1 >= nums.length) {
-            result[j] = result[j - 1]
-        } else if (j === 0) {
-            result[j] = currValue
-        } else {
-            result[j] = currValue * result[j - 1]
-        }
+    let suffix = 1;
+    for (let j = nums.length - 1; j >= 0; j--) {
+        result[j] = result[j] * suffix;
+        suffix = suffix * nums[j];
     }
     return result;
 }
