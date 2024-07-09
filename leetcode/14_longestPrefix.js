86a8a90d3db1e3ef373b501cb6ef077cf6bc5646
Scarlett <hao.sijia04@gmail.com>
2024-07-08 22:28:59 -0400

    description of question

---
+++ leetcode/14_longestPrefix.js
@@ -0,0 +1,16 @@
+// 14_longestPrefix
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    
+};
+
+// Constraints:
+
+// 1 <= strs.length <= 200
+// 0 <= strs[i].length <= 200
+// strs[i] consists of only lowercase English letters.
+
+// Trie 
