4b0d6a1c1f4e9d9c2768afdcf786e24856165e4e
Scarlett <hao.sijia04@gmail.com>
2024-07-09 22:29:37 -0400

    scanning logic

--- leetcode/14_longestPrefix.js
+++ leetcode/14_longestPrefix.js
@@ -3,10 +3,24 @@
  * @param {string[]} strs
  * @return {string}
  */
-var longestCommonPrefix = function(strs) {
+var longestCommonPrefix = function (strs) {
     let charIndex = 0;
+    const output = [];
     for (const strIndex of strs) {
-        if(strs[strIndex])
+        // if root node is different, then there's no common prefix
+        if (strs[strIndex][charIndex] !== strs[strIndex + 1][charIndex]) {
+            return "";
+        } else {
+            // if root nodes are the same, then move to the next root node
+            if (strIndex < strs.length - 2) {
+                strIndex++;
+                continue; // stops statement execution and goes to the next iteration
+            } else {
+                output.push(strs[strIndex][charIndex]);
+                strIndex = 0; // reset the pointer
+            }
+
+        }
     }
 };
 
