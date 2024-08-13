6edbd08fa8c9be3262f20beb0070c867a4438198
Scarlett <hao.sijia04@gmail.com>
2024-08-12 22:46:39 -0400

    slider approach

--- leetcode/28_indexOfFirstOccurence.js
+++ leetcode/28_indexOfFirstOccurence.js
@@ -6,28 +6,28 @@
  * @return {number}
  */
 var strStr = function (haystack, needle) {
-    let firstIndex = null;
-    let needlePointer = 0;
-    let haystackPointer = 0;
-    const needleLength = needle.length;
-    const haystackLength = haystack.length;
+    // iterate all the substring in haystack
+    // open a window of size m, slide it over haystack
+    // needle length is m, haystack length is n
+    // for each substring, it starts at index (m-1) + 0, ends at index (m-1) + k, max at n - 1
+    // so k = n - m
 
-    while (haystackPointer < haystackLength) {
-        if (haystack[haystackPointer] === needle[needlePointer]) {
-            firstIndex = haystackPointer;
-            if (needlePointer === needleLength) {
-                return firstIndex;
+    let hayLength = haystack.length;
+    let neeLegnth = needle.length;
+
+    // open a window in haystack, the range of windowStart is n - m;
+    for (let windowStart = 0; windowStart <= hayLength - neeLegnth; windowStart++) {
+        // iterate the substring, the range is m
+        for (let needlePointer = 0; needlePointer < neeLegnth; needlePointer++) {
+            if (needle[needlePointer] !== haystack[windowStart + needlePointer]) {
+                break;
             }
-            haystackPointer++;
-            needlePointer++;
-        }
-        if (haystack[haystackPointer] !== needle[needlePointer]) {
-            haystackPointer++;
-            needlePointer = 0;
-            if (haystackPointer === haystackLength) {
-                return -1;
+            // if chars in needle and sub-array in haystack are all equal
+            // the needle pointer sucessfully reach the last char
+            if (needlePointer === neeLegnth - 1) {
+                return windowStart;
             }
         }
-        return firstIndex;
     }
-};
+    return - 1;
+}
