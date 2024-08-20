2a8e21d4213df21efc731a637eddc6dff4b63dd9
Scarlett <hao.sijia04@gmail.com>
2024-08-19 21:01:09 -0400

    partly right. while loop logic bug

--- leetcode/209_Min_Size_Subarray_Size.js
+++ leetcode/209_Min_Size_Subarray_Size.js
@@ -9,7 +9,7 @@ var minSubArrayLen = function (target, nums) {
     // keep a running sum
     // adjust the slide window: contract the window if the sum >= target
 
-    let minLength = 0;
+    let minLength = Infinity;
     let sum = 0;
     let left = 0;
     // use a sliding window to represent the current subArray
@@ -17,7 +17,17 @@ var minSubArrayLen = function (target, nums) {
         sum = sum + nums[right];
 
         // if sum >= target
-        sum = sum - nums[left];
-
+        while (sum >= target) {
+            // check the minLength 
+            if (right - left + 1 < minLength) {
+                minLength = right - left + 1;
+            }
+            sum = sum - nums[left]
+            if (sum < target) {
+                break;
+            }
+            left++;
+        }
     }
+    return minLength !== Infinity ? minLength : 0;
 };
