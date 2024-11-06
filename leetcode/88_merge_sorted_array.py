353d4ac4d40aae9f973e41398eb0aefa726edaa3
Scarlett <hao.sijia04@gmail.com>
2024-11-05 19:05:41 -0500

    solution- use three pointers, count from the end, handle remaining

--- leetcode/88_merge_sorted_array.py
+++ leetcode/88_merge_sorted_array.py
@@ -2,17 +2,25 @@
 
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-        merged_arr_pointer = m + n -1
-        
-        while m >=0 and n >= 0:
-            if nums1[m-1] >= nums2[n - 1]:
-                nums1[merged_arr_pointer] = nums1[m-1]
-                m -= m
-                merged_arr_pointer -= merged_arr_pointer
+        if n == 0:
+            return nums1
+        elif m == 0 and n != 0:
+            nums1[:n] = nums2[:n]
+
+        merged_arr_pointer = m + n - 1
+        m = m - 1
+        n = n - 1
+
+        while m >= 0 and n >= 0:
+            if nums1[m] >= nums2[n]:
+                nums1[merged_arr_pointer] = nums1[m]
+                m = m - 1
             else:
-                nums1[merged_arr_pointer] = nums2[n - 1]
-                n -= n
-                merged_arr_pointer -= merged_arr_pointer
+                nums1[merged_arr_pointer] = nums2[n]
+                n = n - 1
+            merged_arr_pointer = merged_arr_pointer - 1
+
+        while n >= 0:
+            nums1[merged_arr_pointer] = nums2[n]
+            n -= 1
+            merged_arr_pointer -= 1
