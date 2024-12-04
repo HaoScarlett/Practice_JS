c4b3286a7b2ececea8957811404e4b9c5f52a75c
Scarlett <hao.sijia04@gmail.com>
2024-12-03 20:41:24 -0500

    left and right pointers; sorted array; 1 indexd

---
+++ leetcode/167_two_sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l = 0
+        r = len(numbers) - 1
+        
+        while l < r:
+            sum = numbers[l] + numbers[r]
+            if sum == target:
+                return  [l + 1, r + 1]
+            elif sum > target:
+                r -= 1
+            else:
+                l += 1
+        # In case there's no solution
+        return [-1, -1]
+        
